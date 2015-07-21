e17bde8fc4a664d1b045a5ab8d7cb3bd3cba2b96
markkoshkin <koshkin_mark@bigmir.net>
2015-07-22 00:33:00 +0300

    Start
    Предложите максимально эффективный алгоритм для поиска всех
    целочисленных делителей числа.
    Входные данные: пользователь вводит число. Результат: список делителей 

---
+++ intNumbers/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace intNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Write a number");
+                int number = Int32.Parse(Console.ReadLine());
+                for (int i = 1; i <= number; i++)
+                {
+                    if ((number % i) == 0)
+                    {
+                        Console.WriteLine(i);
+                    }
+                }
+                Console.ReadLine();
+            }
+            catch (Exception)
+            {
+
+                Console.WriteLine("Error");
+            }
+            
+        }
+    }
+}
